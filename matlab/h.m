7207b3af745a4b3cd98d631751b7980e1efc0558
reginag99 <regina-maria@live.se>
2023-04-24 15:52:21 +0200

    Snyggade till

--- matlab/h.m
+++ matlab/h.m
@@ -1,10 +1,13 @@
 function out = h(T_water, betag_v2, L, Pr_air)%D = diameter of the beaker
+%calculate heat transfer coefficient. Referenses are to the book
+%"fundamentals of momentum, heat and masstransfer"
+
 T_air = 273.15+25;
 deltaT = T_water - T_air;
-Gr = betag_v2 *L^3*deltaT;
-Ra = Gr * Pr_air;
+Gr = betag_v2 *L^3*deltaT;%s.306
+Ra = Gr * Pr_air; % eq 19-3
 
-if (10^5<Ra) && (Ra<2*10^7)
+if (10^5<Ra) && (Ra<2*10^7) %s.302
     C = 0.54;
     x = 1/4;
 elseif (2*10^7<Ra) && (Ra<3*10^(10))
