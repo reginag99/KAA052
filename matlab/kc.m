16631321f8ff4d5288bd85f3243ec2571a167321
rebejon <rebejon@chalmers.se>
2023-04-24 15:50:51 +0200

    Update kc.m

--- matlab/kc.m
+++ matlab/kc.m
@@ -1,6 +1,6 @@
-function out = kc(T)
+function out = kc(T,Pr)
 %mass transfer coefficient for convective mass transfer between water and
 %air 
 %cp, rho will be at Twater+Tair/2
 
-out = (h * (Sc/Pr)^(-2/3)) / (cp_air * rho_air)
+out = (h * (Sc_air/Pr)^(-2/3)) / (cp_air * rho_air)
