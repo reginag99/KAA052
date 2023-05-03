0248e78a482cdbe90b673d386251ea1c7a610070
reginag99 <regina-maria@live.se>
2023-05-03 13:48:03 +0200

    Uppdaterat

--- matlab/Sc_air.m
+++ matlab/Sc_air.m
@@ -2,4 +2,4 @@ function out = Sc_air(T,DAB)
 %function that solves for Schmidt number 
 %DAB = mass diffusion rate
 %T=Twater/Tair/2
-out = my_air/(rho_air * DAB)
+out = my_air(T)/(rho_air(T) * DAB);
--- matlab/h.m
+++ matlab/h.m
@@ -2,19 +2,22 @@ function out = h(T_water_glas, betag_v2, L, Pr_air)%D = diameter of the beaker
 %calculate heat transfer coefficient. Referenses are to the book
 %"fundamentals of momentum, heat and masstransfer"
 
-T_air = 273.15+25;
-deltaT = T_water - T_air;
+T_air = 300;
+deltaT = T_water_glas - T_air;
 Gr = betag_v2 *L^3*deltaT;%s.306
 Ra = Gr * Pr_air; % eq 19-3
 
-if (10^5<Ra) && (Ra<2*10^7) %s.302
+if (10^4<Ra) && (Ra<2*10^7) %s.302
     C = 0.54;
     x = 1/4;
-elseif (2*10^7<Ra) && (Ra<3*10^(10))
+elseif (2*10^7<Ra) && (Ra<3*10^(19))
     C = 0.14;
+    x = 1/3;
+else
+    C = 0.5;
     x = 1/3;
 end
 
-out = C*Ra^x*k_air/L;
+out = C*Ra^x*k_air(T_water_glas)/L;
 
 end
