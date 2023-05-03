c2bc92e9656887dd27e204e5fcd9dfd32f806d6a
reginag99 <regina-maria@live.se>
2023-05-03 21:41:41 +0200

    uppdaterat

--- matlab/dT_dt_G.m
+++ matlab/dT_dt_G.m
@@ -13,7 +13,7 @@ eps = 0.95;
 r_outer = 70*10^-3;
 r_inner = 63*10^-3;
 T_air = 300;
-D_AB = 2.634/p_water((T_water+T_air)/2); %ändraaaaaaaa plz
+D_AB = 2.634/101325;%p_water((T_water+T_air)/2); %ändraaaaaaaa plz
 R = 8.3145;
 M = (1.0079*2 + 16)*10^-3;
 
@@ -26,14 +26,14 @@ L_water=125.81*10^-3/(rho_water(273.15+78.6)*2*r_inner*pi);
 T0=[T_water-5];%initial guess for T_surf
 options = optimoptions('fsolve','Display','none');
 T_surf=fsolve(@(T_surf)Cond(T_surf,T_water,T_air,L),T0,options);
-k_c = h_top(T_water,T_air,betag_v2,r_inner)*(Sc_air((T_water + T_air)/2, D_AB)*pr_air(T_air,T_water))^(-2/3)/(cp_water(T_water)*rho_water((T_water+T_air)/2));
-c_surf = p_water(T_air)/(R*T_air);
+%k_c = h_top(T_water,T_air,betag_v2,r_inner)*(Sc_air((T_water + T_air)/2, D_AB)*pr_air(T_air,T_water))^(-2/3)/(cp_water(T_water)*rho_water((T_water+T_air)/2));
+c_surf = p_water((T_air+T_water)/2)*0.22/(R*(T_air+T_water)/2);
 c_bulk = rho_water(T_water)/M;
-
+k_c = kc(T_water,T_air,betag_v2, r_inner,D_AB);
 
 %derivator
 dTwater_dt =(-h_cyl(T_surf,T_air,betag_v2,L,r_outer)*A_surf*(T_surf-T_air) -h_top(T_water,T_air,betag_v2,r_inner)*A_top*(T_water-T_air) -sigma*eps*A_surf*(T_surf^4 - T_air^4))/(rho_water(T_water)*cp_water(T_water)*r_inner*2*pi*L_water);  %(-dTsurf_dt+h_surf*A_surf*(T_surf-T_air) + h_top*A_top*(T_water-T_air)+sigma*eps*A_surf*(T_surf^4 - T_air^4))/(rho_water(T_water)*cp_water(T_water)*r_inner*2*pi*L);
-dm_dt = k_c*(c_surf-c_bulk);
+dm_dt = k_c*(c_surf-c_bulk)*M*A_top;
 
 dT_dt = [dTwater_dt;dm_dt];
 end
--- matlab/kc.m
+++ matlab/kc.m
@@ -1,6 +1,11 @@
-function out = kc(T,Pr)
+function out = kc(T_water,T_air,betag_v2,r_inner,DAB)
 %mass transfer coefficient for convective mass transfer between water and
 %air 
 %cp, rho will be at Twater+Tair/2
+T = (T_water+T_air)/2; 
+h = h_top(T_water,T_air,betag_v2,r_inner);
+Sc = Sc_air(T,DAB);
+Pr = pr_air(T_air,T_water);
+Cp_rho = cp_air(T)*rho_air(T);
 
-out = (h * (Sc_air/Pr)^(-2/3)) / (cp_air * rho_air)
+out = h*(Pr/Sc)^(2/3)/Cp_rho;
