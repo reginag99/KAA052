f95b4dca28e907f15b3425c7d29ba7d7128b1a19
reginag99 <regina-maria@live.se>
2023-05-12 14:11:34 +0200

    Nya filer

---
+++ matlab/test_variation_RG.m
@@ -0,0 +1,67 @@
+clear; clc; clf; close  all
+S=readtable("small_beaker_3.txt");
+T_water0 = S{1,2}+273.15;
+m0 = S{1,3}*10^-3;
+r_outer = 70*10^-3;
+r_inner = 63*10^-3;
+
+
+T0 = [T_water0,m0];%[T_surf0, T_water0,(T_surf0-T_water0)/(r_outer-r_inner)];%T_surf, T_water
+t_span = S{:,1};
+selsctiontype = ['r_inner', 'r_outer', 'T_air', 'L'];
+Select = 1;
+
+
+%T-plots
+[t,T] = ode45(@dT_dt_RG2,t_span,T0,[],Select);
+subplot(2,1,1)
+plot(t,T(:,1)); hold on;
+a=S{:,2}+273.15;
+plot(t,a);
+legend('Model Twater (K)','Exp Twater (K)'); 
+
+%mass plots
+
+subplot(2,1,2)
+plot(t,T(:,2)); hold on;
+legend('Mass')
+
+m=S{:,3}*10^-3;
+plot(t,m); 
+legend('Model mass(g)','Exp mass(g)');
+
+%ser bra ut, men hon skulle rekommendera att vi lägger till
+%värmeöverföring från avdunstningen
+
+% 
+% MT = [T(:,1),S{:,2}+273.15];
+% MT_name = {'Genereted data', 'Real data'};
+% [p,t,stats] = anova1(MT,MT_name);
+% 
+% R2_T = 1-sum((S{:,2}+273.15-T(:,1)).^2)/sum((S{:,2}+273.25-mean(S{:,2}+273.15)).^2)
+% Rest_T = (S{:,2}+273.15-T(:,1))';
+% 
+% 
+% Mm = [T(:,2),S{:,3}*10^(-3)];
+% Mm_name = {'Genereted data, mass', 'Real data, mass'};
+% [p,t,stats] = anova1(Mm,Mm_name);
+% 
+% R2_m = 1-sum((S{:,3}*10^-3-T(:,2)).^2)/sum((S{:,3}*10^-3-mean(S{:,3}*10^-3)).^2)
+% Rest_m = (S{:,3}*10^-3-T(:,2))';
+% 
+% figure
+% subplot(2,1,1)
+% plot(t_span,Rest_T,'o')
+% yline(0,'r--')
+% xlabel('Time, [s]')
+% ylabel('Residuals')
+% title('Plot over residual for heattransfer')
+% 
+% 
+% subplot(2,1,2)
+% plot(t_span,Rest_m,'o')
+% yline(0,'r--')
+% xlabel('Time, [s]')
+% ylabel('Residuals')
+% title('Plot over residual for masstransfer')
+% 
