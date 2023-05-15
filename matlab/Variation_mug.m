da56a1d59a4dc5d1ed9d43c09e1446ccaca659ad
reginag99 <regina-maria@live.se>
2023-05-15 15:15:06 +0200

    nytt

---
+++ matlab/Variation_mug.m
@@ -0,0 +1,61 @@
+%The plot shows the heat and mass transfer for different values of the
+%constants. The constants that can vary in the ones mentioned in
+%'selectiontype', 'Select' refers to the constant you want to vary and 'I'
+%is the number of different values you want to include in the plot. 
+%The max and min values for each constant that will be tested can be found 
+% in the file 'dT_dt_RG2.m' The values of the constant is illustrated by 
+% a colourgradient. 
+
+%Lighter colour = low values of constant
+%Darker colour = high values of constant
+
+
+clear; clc; clf; close  all
+T_water0 = 90+273.15;
+m0 = 150*10^-3;
+T0 = [T_water0,m0]; %startvalues for the ode
+t_span =[0:9000];
+t_h = t_span(end)/3600
+
+selctiontype = [{'T_{air}'}, {'r_{inner}'}, {'r_{outer}'}, {'L'}];%constants that can change. Choose one of them with 'Select'
+Select = 4; %The constant that will vary. 1 = T_air, 2 = r_inner ect
+I = 7; %Number of different values of constant you want to include in the plot
+
+
+figure("Name", "Variatin of constant  " + selctiontype(Select))
+subplot(2,1,1)
+xlabel('Time, [s]','FontSize',12,'FontWeight','bold')
+ylabel('Temperature, [K]','FontSize',12,'FontWeight','bold')
+hold on
+subplot(2,1,2)
+xlabel('Time, [s]','FontSize',12,'FontWeight','bold')
+ylabel('Mass, [g]','FontSize',12,'FontWeight','bold')
+hold on
+
+Green_light = [60,200,160]/255;
+Green_dark = [10,130,90]/255;
+
+Orange_light = [255,130,30]/255;
+Orange_dark = [180,60,0]/255;
+
+
+
+
+for iter = 1:I
+    Green_grad = @(iter) Green_light + (Green_dark - Green_light)/I*iter;
+    Orange_grad = @(iter) Orange_light + (Orange_dark - Orange_light)/I*iter;
+
+    It_opt = [Select,I,iter];
+    [t,T] = ode45(@(t,T) dT_dt_mug(t,T,It_opt,m0),t_span,T0);
+
+    %Temp plots
+    subplot(2,1,1)
+    plot(t,T(:,1),'Color',Green_grad(iter),'linewidth',1); hold on;
+    title(['Variation analysis for ', selctiontype{Select}, ', heat transfer'])
+    yline(273.15+50, 'r--', LineWidth=1)  
+    %mass plots
+    subplot(2,1,2)
+    plot(t,T(:,2),'Color',Orange_grad(iter),'linewidth',1); hold on;
+    title(['Variation analysis for ' selctiontype{Select} ', mass transfer'])
+end
+
---
+++ matlab/dT_dt_mug.m
@@ -0,0 +1,77 @@
+function dT_dt = dT_dt_mug(t,T,It_opt,m0)
+%Variables
+select = It_opt(1);%Constant that will change
+I = It_opt(2);%number of different values of constant
+iter = It_opt(3);%Which iteration of the constant
+T_water = T(1);
+m = T(2);
+
+
+%def constants, baseline
+k_glas = 0.78;
+sigma = 5.67*10^-8;
+eps = 0.95;
+L=95*10^-3;
+T_air = 20.6+273.15;
+r_outer = 70*10^-3;
+r_inner = 63*10^-3;
+D_AB = 2.634/101325;%p_water((T_water+T_air)/2); %ändraaaaaaaa plz
+R = 8.3145;
+M = (1.0079*2 + 16)*10^-3;
+
+Tair_min = 273.15 + 15;
+Tair_max = 273.15 + 25;
+dT = (Tair_max-Tair_min)/I;
+
+rinner_min = 50*10^-3/2;
+rinner_max = 70*10^-3/2;
+drinner = (rinner_max-rinner_min)/I;
+
+router_min = 50*10^-3/2;
+router_max = 70*10^-3/2;
+drouter = (router_max-router_min)/I;
+
+L_min = 80*10^-3;
+L_max = 105*10^-3;
+dL = (L_max - L_min)/I;
+
+if select == 1
+          T_air = Tair_min + dT*iter;
+elseif select == 2
+            r_inner = rinner_min + drinner*iter;
+            r_outer = r_inner + 5*10^-3;  %for the mug
+            L = 150*10^-6/(r_inner^2*pi);  %for the mug
+elseif select == 3
+            r_outer = router_min + drouter*iter;
+            r_inner = r_outer - 5*10^-3; %for the mug
+            L = 150*10^-6/(r_inner^2*pi);  %for the mug
+elseif select == 4
+            L = L_min + dL*iter;
+            r_inner = sqrt(150*10^-6/(L*pi));  %for the mug
+            r_outer = r_inner + 5*10^-3;  %for the mug
+end
+    
+
+beta = betag_v2(T_air);
+A_surf = r_outer*2*pi*L;
+A_top = r_inner^2*pi;
+L_water=m0/(rho_water(273.15+78.6)*2*r_inner*pi); 
+
+T0=[T_water-5];%initial guess for T_surf
+options = optimoptions('fsolve','Display','none');
+T_surf=fsolve(@(T_surf)Cond(T_surf,T_water,T_air,L,beta),T0,options);
+
+c_surf = p_water(T_air)*0.22/(R*T_air);
+c_bulk = p_water((T_air+T_water)/2)/(R*(T_air+T_water)/2); %sida/kap 499/26, vi hade skrivit bulkkoncentrationen fel. Det är från vattenytan det evaporerar, därmed bör ångtrycket vid vattenytan användas som koncentration
+k_c = kc(T_water,T_air,beta, r_inner,D_AB);
+
+%derivator
+dm_dt = k_c*(c_surf-c_bulk)*M*A_top;%/(1-c_surf);
+rad = sigma*eps*A_surf*(T_surf^4 - T_air^4);
+conv_heat = h_cyl(T_surf,T_air,beta,L,r_outer)*A_surf*(T_surf-T_air) +h_top(T_water,T_air,beta,r_inner)*A_top*(T_water-T_air);
+conv_mass = dHvap_water(T_water)*dm_dt;
+
+dTwater_dt =(-conv_mass-conv_heat -rad)/(rho_water(T_water)*cp_water(T_water)*r_inner*2*pi*L_water);  
+
+dT_dt = [dTwater_dt;dm_dt];
+end
