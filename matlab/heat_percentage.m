2648b046404110ef95a17fc52f0c8f80a71fcd7c
rebejon <rebejon@chalmers.se>
2023-05-17 14:01:35 +0200

    procent

--- matlab/heat_percentage.m
+++ matlab/heat_percentage.m
@@ -62,29 +62,29 @@ evaporation = heat(:,4)./heat_tot;
 subplot(3,2,1)
 plot(t,radiation,'g','LineWidth',1);
 xlabel('Time (s)');
-ylabel('q_r_a_d (%)');
+ylabel('q_r_a_d/q_t_o_t (%)');
 legend('Radiation');
 
 subplot(3,2,2);
 plot(t,convection_walls,'r','LineWidth',1);
 xlabel('Time (s)');
-ylabel('q_w_a_l_l_s (%)');
+ylabel('q_w_a_l_l_s/q_t_o_t (%)');
 legend('Convection beaker walls');
 
 subplot(3,2,3);
 plot(t,convection_top,'b','LineWidth',1);
 xlabel('Time (s)');
-ylabel('q_t_o_p (%)');
+ylabel('q_t_o_p/q_t_o_t (%)');
 legend('Convection beaker top');
 
 subplot(3,2,4);
 plot(t,conduction,'m','LineWidth',1);
 xlabel('Time (s)');
-ylabel('q_c_o_n_d (%)');
+ylabel('q_c_o_n_d/q_t_o_t (%)');
 legend('Conduction');
 
 subplot(3,2,5);
 plot(t,evaporation,'k','LineWidth',1);
 xlabel('Time (s)');
-ylabel('q_e_v_a_p (%)');
+ylabel('q_e_v_a_p/q_t_o_t (%)');
 legend('Evaporation');
