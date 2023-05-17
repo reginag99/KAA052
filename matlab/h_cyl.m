f3286cc2af3b330d19b18c4bdfce7ea8566389cb
reginag99 <regina-maria@live.se>
2023-05-17 13:31:25 +0200

    Uppdaterat

--- matlab/h_cyl.m
+++ matlab/h_cyl.m
@@ -9,7 +9,7 @@ Ra = Gr * pr_air(T_air,T_surf); % eq 19-3
 
 %if 2*r_outer > 35/Gr^(1/4)
     Nu = 0.68 + 0.67*Ra^(1/4)/(1+(0.492/pr_air(T_air,T_surf)));
-    out = Nu*k_air(T_surf)/L;
+    out = Nu*k_air((T_surf+T_air)/2)/L;
 %else
  %   error('Nu can not be calculated as a vertical cylinder')
 %%end
